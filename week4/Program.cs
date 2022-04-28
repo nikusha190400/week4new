101f91aa8364c4818a79218edc30c9c0d20c0c87
nikusha190400 <katapariashvili@outlook.com>
2022-04-28 17:07:59 +0400

    first commit

---
+++ week4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace week4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("katapariashvili nikoloz");
+        }
+    }
+}
